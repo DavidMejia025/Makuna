db14e8505dd437ed1e71c46d6fc62b1c3f39ca90
Manuel Ortega <manuel@lasindias.coop>
2018-08-04 20:32:54 -0500

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20180805001831) do
+
   create_table "ckeditor_assets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "data_file_name", null: false
     t.string "data_content_type"
