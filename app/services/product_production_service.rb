8d5073e28c0242b0a76ee4f0e08b2272aede0788
Manuel Ortega <manuel@lasindias.coop>
2018-08-05 08:08:34 -0500

    Added Dashboard to the menu page

--- app/services/product_production_service.rb
+++ app/services/product_production_service.rb
@@ -1,5 +1,5 @@
-class ProductPerformanceService < BaseService
-  API_ENPOINT = 'https://makuna-predictor.herokuapp.com/makuna/performance'
+class ProductProductionService < BaseService
+  API_ENPOINT = 'https://makuna-predictor.herokuapp.com/makuna/production'
 
   def initialize(product_name, department, year)
     @product_name = product_name
@@ -7,7 +7,7 @@ class ProductPerformanceService < BaseService
     @year = year
   end
 
-  def performance
+  def production
 
     params_hash ={}
     #params_hash[:id] = 0
@@ -15,8 +15,8 @@ class ProductPerformanceService < BaseService
     params_hash[:department] = @department
     params_hash[:year] = @year
     #params_hash[:product_id] = 0
-    params_hash[:production] = nil
     params_hash[:performance] = nil
+    params_hash[:production] = nil
 
     @result = HTTParty.post(API_ENPOINT,
       :headers => {'Content-Type'=>'application/json'},
